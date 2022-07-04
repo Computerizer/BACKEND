3b2943d91a5fe999a2778fd342f3f4ba61b7b96e
Emad Eldin Gasser <omda.g@outlook.com>
2022-07-04 17:58:06 +0200

    🔒 ENV variables

--- Computerizer/Computerizer/settings.py
+++ Computerizer/Computerizer/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,8 +80,12 @@ WSGI_APPLICATION = 'Computerizer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('COMPUTERIZER-DATABASE-NAME'),
+        'USER': 'fbvcjwibxrtfef',
+        'PASSWORD': os.environ.get('COMPUTERIZER-DATABASE-PASS'),
+        'HOST': 'ec2-34-233-115-14.compute-1.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
