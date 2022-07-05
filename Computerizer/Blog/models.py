1d4f69f47fb4ce980b39292a20be33cfe8ca163b
Yousef Mahmoud <mryoyo133@gmail.com>
2022-07-05 19:20:26 +0200

    setting up the database

--- Computerizer/Blog/models.py
+++ Computerizer/Blog/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+from distutils.command.upload import upload
+from operator import mod
+from django import views
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+# Create your models here.
+class Author(AbstractUser):
+    pass
+
+class Post(models.Model):
+    title = models.CharField(max_length=258)
+    #author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    body = models.TextField()
+    image = models.ImageField(upload_to = r'Computerizer/static/Blog/media')
+    upload_date  = models.DateTimeField(auto_now_add=True)
+    likes = models.PositiveIntegerField(default=0)
+    views = models.PositiveIntegerField(default=0)
+
+class Comment(models.Model):
+    #user = models.ForeignKey(Author, on_delete=models.CASCADE)    
+    body = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    upload_date = models.DateTimeField(auto_now_add=True)
+    likes = models.PositiveIntegerField(default=0)
