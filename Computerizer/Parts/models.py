02f642afdffe80b9b3ea98b981aa1758f2440eeb
Emad Eldin Gasser <omda.g@outlook.com>
2022-07-16 17:35:31 +0200

    Added more fields to models

--- Computerizer/Parts/models.py
+++ Computerizer/Parts/models.py
@@ -1,47 +1,108 @@
 from random import choices
 from django.db import models
 
-# Create your models here.
-
 
 class Manufacturer(models.Model):
-    Manufacturers = [
+    Manufacturers = (
+        ('AMD', 'AMD'),
+        ('Aorus', 'Aorus'),
+        ('Asus', 'Asus'),
+        ('Colorful', 'Colorful'),
+        ('Cooler Master', 'Cooler Master'),
+        ('Corsair', 'Corsair'),
+        ('Crucial', 'Crucial'),
+        ('EVGA', 'EVGA'),
+        ('Gigabyte', 'Gigabyte'),
+        ('Gskill', 'Gskill'),
+        ('Intel', 'Intel'),
+        ('Lian Li', 'Lian Li'),
         ('MSI', 'MSI'),
-        ('Asus', 'Asus')
-    ]
-    Name = models.CharField()
-    Range = models.CharField()
+        ('Noctua', 'Noctua'),
+        ('Nvidia', 'Nvidia'),
+        ('NZXT', 'NZXT'),
+        ('PNY', 'PNY'),
+        ('PowerColor', 'PowerColor'),
+        ('Razer', 'Razer'),
+        ('Sapphire', 'Sapphire'),
+        ('SAMSUNG', 'SAMSUNG'),
+        ('TeamGroup', 'TeamGroup'),
+        ('Thermaltake', 'Thermaltake'),
+        ('Vcolor', 'Vcolor'),
+        ('XFX', 'XFX'),
+        ('XPG', 'XPG'),
+        ('Zotac', 'Zotac'),
+    )
+    Id = models.CharField(primary_key=True, null=False, blank=False)
+
+    Parent = models.CharField(choices=Manufacturers, max_length=20, null=True, blank=True, help_text='If a Graphics card is a "XFX Radeon 6600XT" for example, \
+    XFX goes into the Name field, and AMD goes into the Parent field')
+
+    Name = models.CharField(choices=Manufacturers, max_length=20, null=True, blank=True, help_text='If a Graphics card is a "XFX Radeon 6600XT" for example, \
+    XFX goes into the Name field, and AMD goes into the Parent field')
+
+    CPUs = models.BooleanField(verbose_name='Manufactures CPU')
+    GPUs = models.BooleanField(verbose_name='Manufactures GPU')
+    Motherboards = models.BooleanField(
+        verbose_name='Manufactures Motherboards')
+    RAM = models.BooleanField(verbose_name='Manufactures RAM')
+    PSU = models.BooleanField(verbose_name='Manufactures PSU')
+    Case = models.BooleanField(verbose_name='Manufactures Cases')
+    Cooler = models.BooleanField(verbose_name='Manufactures Coolers')
 
 
 class CPU_Links(models.Model):
-    Id = models.ForeignKey('CPU')
-    Amazon_URL = models.TextField()
-    Newegg_URL = models.TextField()
-    BestBuy_URL = models.TextField()
+    Id = models.ForeignKey(
+        'CPU', on_delete=models.CASCADE, null=False, blank=False)
+    Amazon_URL = models.URLField()
+    Newegg_URL = models.URLField()
+    BestBuy_URL = models.URLField()
     pass
 
 
 class CPU(models.Model):
-    Id = models.CharField()
-    Manufacturer = models.ForeignKey('Manufacturer')
+    Sockets = (
+        ('AM4', 'AM4'),
+        ('LGA', 'LGA'),
+        ('LGA', 'LGA'),
+        ('LGA', 'LGA'),
+        ('LGA', 'LGA'),
+        ('LGA', 'LGA'),
+    )
+    Id = models.CharField(primary_key=True, null=False, blank=False)
+    Manufacturer = models.ForeignKey('Manufacturer', on_delete=models.CASCADE)
     Model_Name = models.CharField()
     Cores = models.PositiveIntegerField()
     Threads = models.PositiveIntegerField()
     Speed = models.FloatField()
-    Socket = models.CharField()
+    Socket = models.CharField(choices=Sockets, max_length=10)
     Power_Consumption = models.PositiveIntegerField()
     Previous_Price = models.FloatField()
     Current_Price = models.FloatField()
-    Links = models.ForeignKey('CPU_Links')
-    Lowest_Price_Link = models.ForeignKey('Links')
+    Links = models.ForeignKey('CPU_Links', on_delete=models.PROTECT)
+    Lowest_Price_Link = models.URLField()
+    pass
+
+
+class Cooler_Links(models.Model):
+    Id = models.ForeignKey(
+        'Cooler', on_delete=models.CASCADE, null=False, blank=False)
+    Amazon_URL = models.URLField()
+    Newegg_URL = models.URLField()
+    BestBuy_URL = models.URLField()
+    pass
+
+
+class Cooler(models.Model):
+
     pass
 
 
 class GPU_Links(models.Model):
-    Id = models.ForeignKey('GPU')
-    Amazon_URL = models.TextField()
-    Newegg_URL = models.TextField()
-    BestBuy_URL = models.TextField()
+    Id = models.ForeignKey(
+        'GPU', on_delete=models.CASCADE, null=False, blank=False)
+    Amazon_URL = models.URLField()
+    Newegg_URL = models.URLField()
+    BestBuy_URL = models.URLField()
     pass
 
 
@@ -50,10 +111,11 @@ class GPU(models.Model):
 
 
 class Motherboard_Links(models.Model):
-    Id = models.ForeignKey('Motherboard')
-    Amazon_URL = models.TextField()
-    Newegg_URL = models.TextField()
-    BestBuy_URL = models.TextField()
+    Id = models.ForeignKey(
+        'Motherboard', on_delete=models.CASCADE, null=False, blank=False)
+    Amazon_URL = models.URLField()
+    Newegg_URL = models.URLField()
+    BestBuy_URL = models.URLField()
     pass
 
 
@@ -62,10 +124,11 @@ class Motherboard(models.Model):
 
 
 class RAM_Links(models.Model):
-    Id = models.ForeignKey('RAM')
-    Amazon_URL = models.TextField()
-    Newegg_URL = models.TextField()
-    BestBuy_URL = models.TextField()
+    Id = models.ForeignKey(
+        'RAM', on_delete=models.CASCADE, null=False, blank=False)
+    Amazon_URL = models.URLField()
+    Newegg_URL = models.URLField()
+    BestBuy_URL = models.URLField()
     pass
 
 
@@ -74,10 +137,11 @@ class RAM(models.Model):
 
 
 class PSU_Links(models.Model):
-    Id = models.ForeignKey('PSU')
-    Amazon_URL = models.TextField()
-    Newegg_URL = models.TextField()
-    BestBuy_URL = models.TextField()
+    Id = models.ForeignKey(
+        'PSU', on_delete=models.CASCADE, null=False, blank=False)
+    Amazon_URL = models.URLField()
+    Newegg_URL = models.URLField()
+    BestBuy_URL = models.URLField()
     pass
 
 
@@ -86,10 +150,11 @@ class PSU(models.Model):
 
 
 class Case_Links(models.Model):
-    Id = models.ForeignKey('Case')
-    Amazon_URL = models.TextField()
-    Newegg_URL = models.TextField()
-    BestBuy_URL = models.TextField()
+    Id = models.ForeignKey(
+        'Case', on_delete=models.CASCADE, null=False, blank=False)
+    Amazon_URL = models.URLField()
+    Newegg_URL = models.URLField()
+    BestBuy_URL = models.URLField()
     pass
 
 
