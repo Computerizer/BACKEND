2eac0230dae1eec79e7aad8808fce04beba8e819
Emad Eldin Gasser <omda.g@outlook.com>
2022-07-16 17:35:50 +0200

    Registered models to admin page

--- Computerizer/Parts/admin.py
+++ Computerizer/Parts/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+admin.register('CPU_Links')
+admin.register('CPU')
+
+admin.register('Cooler_Links')
+admin.register('Cooler')
+
+admin.register('GPU_Links')
+admin.register('GPU')
+
+admin.register('RAM_Links')
+admin.register('RAM')
+
+admin.register('Motherboard_Links')
+admin.register('Motherboard')
+
+admin.register('PSU_Links')
+admin.register('PSU')
+
+admin.register('Case_Links')
+admin.register('Case')
